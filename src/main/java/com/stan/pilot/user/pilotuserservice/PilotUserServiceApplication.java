50572496e32f83cd27f514d896ea26e0f33645d0
Stanislav Zhurich <stanislav_zhurich@epam.com>
2023-05-18 14:54:22 +0300

    initial commit

---
+++ src/main/java/com/stan/pilot/user/pilotuserservice/PilotUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stan.pilot.user.pilotuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilotUserServiceApplication.class, args);
+	}
+
+}
