f696aff93ff805efd9e5e1c0a7c9d89ebd749b1f
Stanislav Zhurich <stanislav_zhurich@epam.com>
2023-06-24 00:26:04 +0300

    ingress and logs

--- src/main/java/com/stan/pilot/user/controller/UserController.java
+++ src/main/java/com/stan/pilot/user/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,10 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.stan.pilot.user.domain.User;
 import com.stan.pilot.user.service.UserService;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -26,11 +29,13 @@ public class UserController {
 
     @GetMapping("/users/{tenantId}")
     public Flux<User> getUsers(@PathVariable String tenantId) {
+        log.info("requesting users for tenant:" + tenantId);
         return userService.getUsers(tenantId);
     }
 
     @GetMapping("/users/{tenantId}/{userId}")
     public Mono<User> getUserById(@PathVariable("tenantId") String tenantId, @PathVariable String userId) {
+        log.info("requesting user by tenant:" + tenantId + " and id:" + userId);
         return userService.findById(userId, tenantId);
     }
 
