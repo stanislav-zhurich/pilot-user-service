9c324b3541ab8bc715015f6cd72f5174b91b2bbe
Stanislav Zhurich <stanislav_zhurich@epam.com>
2023-07-01 19:28:46 +0300

    change logging

--- src/main/java/com/stan/pilot/user/PilotUserServiceApplication.java
+++ src/main/java/com/stan/pilot/user/PilotUserServiceApplication.java
@@ -3,11 +3,21 @@ package com.stan.pilot.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class PilotUserServiceApplication{
 
 
 	public static void main(String[] args) {
+		log.info("Hi");
+		String id = System.getenv("service_sp_id");
+		log.info("id:" + id);
+		String secret = System.getenv("service_sp_pswd");
+		log.info("secret:" + secret);
+		String tenant = System.getenv("service_sp_tenant_id");
+		log.info("tenant:" + tenant);
 		SpringApplication.run(PilotUserServiceApplication.class, args);
 	} 
 }
